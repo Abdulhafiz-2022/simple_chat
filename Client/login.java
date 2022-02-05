c2808526c4809fd5ae3b3df6578d6601d0ad5ce4
Abdulhafiz-2022 <81434019+bedasa49@users.noreply.github.com>
2022-02-06 00:35:11 +0300

    login class is created

--- Client/login.java
+++ Client/login.java
@@ -1,1 +1,10 @@
 package Client;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class login
